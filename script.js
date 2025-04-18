e28ff00c272e5fab55bc4686ae4779fabae59866
Jonas <92853498+joraka@users.noreply.github.com>
2025-04-18 11:47:51 +0300

    working game

--- script.js
+++ script.js
@@ -3,58 +3,76 @@ const startBtnEl = document.getElementById("startBtn");
 const difficultySelectEl = document.getElementById("difficulty");
 const statusEl = document.getElementById("status");
 
-const emojis = ["🐶", "🍕", "🚗", "🌈", "🐱", "🍎", "🎵", "⚽"];
-const pairs = 12;
-
-function shuffleArr(arr) {
-  return arr.sort(() => Math.random() - 0.5);
-}
-
-const lastOpenedCards = new Set();
-
+const shuffleArr = (arr) => arr.sort(() => Math.random() - 0.5);
 const randomBool = () => Math.floor(Math.random() <= 0.5);
 
-startBtnEl.addEventListener("click", () => {
-  start();
-});
+const emojis = ["🐶", "🍕", "🚗", "🌈", "🐱", "🍎", "🎵", "⚽"];
 
-function onCardClick(event) {
-  const card = event.target;
-  if (lastOpenedCards.size < 2 && !lastOpenedCards.has(card)) {
-    card.classList.add("flipped");
-    lastOpenedCards.add(card);
-
-    if (lastOpenedCards.size === 2) {
-      setTimeout(() => {
-        unfipCards();
-      }, 1000);
-    }
-  }
-}
-
-function unfipCards() {
-  for (const card of lastOpenedCards) {
-    card.classList.remove("flipped");
-    lastOpenedCards.delete(card);
-  }
-}
+const lastOpenedCards = new Set();
+const correctCards = new Set();
+let shuffledEmojiList = [];
 
 function start() {
   lastOpenedCards.clear();
-  const currentEmojis = shuffleArr([...emojis, ...emojis]);
+  correctCards.clear();
+  shuffledEmojiList = shuffleArr([...emojis, ...emojis]);
 
   boardEl.innerHTML = "";
 
-  currentEmojis.forEach((emoji) => {
+  shuffledEmojiList.forEach((emoji) => {
     const card = document.createElement("div");
-    card.classList.add("card");
+    card.classList.add("card", "flipped");
 
-    const cardEmoji = card.appendChild(document.createElement("div"));
-    cardEmoji.innerText = emoji;
+    const cardEmojiEl = card.appendChild(document.createElement("div"));
+    card._emojiEl = cardEmojiEl;
+    card._emoji = emoji;
+    // cardEmoji.innerText = emoji;
 
     card.addEventListener("click", onCardClick);
     boardEl.appendChild(card);
   });
 }
 
+//button events
+startBtnEl.addEventListener("click", () => {
+  start();
+});
+
+function onCardClick(event) {
+  const card = event.target;
+  if (lastOpenedCards.size < 2 && !lastOpenedCards.has(card) && !correctCards.has(card)) {
+    card.classList.remove("flipped");
+    card._emojiEl.innerText = card._emoji;
+    lastOpenedCards.add(card);
+
+    //2 cards opened at the same time
+    if (lastOpenedCards.size == 2) {
+      const [firstCard, secondCard] = Array.from(lastOpenedCards);
+      //if correct guess
+      if (firstCard._emoji === secondCard._emoji) {
+        correctCards.add(firstCard);
+        correctCards.add(secondCard);
+        firstCard.classList.add("matched");
+        secondCard.classList.add("matched");
+        lastOpenedCards.clear();
+
+        if (shuffledEmojiList.length === correctCards.size) {
+          alert("you won");
+        }
+      } else {
+        // flip cards back down
+        setTimeout(() => {
+          for (const card of lastOpenedCards) {
+            card.classList.add("flipped");
+            // hide emoji
+            card._emojiEl.innerHTML = "";
+            lastOpenedCards.delete(card);
+          }
+        }, 750);
+      }
+    }
+  }
+}
+
+//init
 start();
